873327619313aa101a61e0f17b20a1103adb7177
devina-agr <devinaagrawal2005@gmail.com>
2026-02-17 15:28:00 +0530

    new commit

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,26 @@
 import http from 'http';
+import os from "os"
+
 const server = http.createServer((req,res) => {
     const url = req.url;
-    if (url == "/") {
+    const method = req.method;
+    if (url == "/" && method=="GET") {
         res.write("Home Page")
     }
-    else if (url == "/about") {
-        res.write("About Page")
+    else if (url == "/system" && method=="GET") {
+        res.write("System")
     }
-    else if (url == "/contact") {
+    else if (url == "/contact" && method=="GET") {
         res.write("Contact Page")
     }
+    else if (url == "/createuser" && method=="POST") {
+        res.write("Create user")
+    }    
+    else if (url.startsWith("/users/") && method=="GET") {
+        res.write("List of users")
+    }
     else {
+        res.statusCode = 404;
         res.write("Error Page")
     }
     res.end();
